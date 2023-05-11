1cc62e2544c21908de42fe793ca61d7e295bbbc1
Jay7735 <jaydeepdhillon@hotmail.co.uk>
2023-05-11 09:53:59 +0100

    should make article by id get work

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
-const { getTopics, getEndpoints } = require("./controllers");
+const { getTopics, getEndpoints, getArticleById } = require("./controllers");
+
+
+
 
 app.get("/api/topics", getTopics);
 
 app.get('/api', getEndpoints);
 
+app.get('/api/articles/:article_id', getArticleById)
+
 app.all('*', (req, res)=>{
     res.status(404).send({msg: '404 Not Found'})
 })
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  else if(err.code === '22P02') {
+    res.status(400).send({ msg: 'Invalid input' });
+  } else res.status(500).send({msg: 'Internal Server Error' });
+});
 
 module.exports = app;
